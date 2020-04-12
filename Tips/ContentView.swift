11b4dfc124705ba10177e8c2fb5a6c9cf37e1685
Brayhan De Aza <brayhandeaza@gmail.com>
2020-04-12 19:01:41 -0400

    Initial Commit

---
+++ Tips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tips
+//
+//  Created by Anton on 4/12/20.
+//  Copyright © 2020 Brayhan De Aza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
